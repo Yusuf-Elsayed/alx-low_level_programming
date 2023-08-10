ce88c658f31e1b50e7d2f1f1faa4dfab484d3990
Yusuf-Elsayed <yusufElsayed@gmail.com>
2023-08-10 03:32:21 -0700

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - Entry point/
+ *
+ * Description: Program to print
+ *
+ * Return: 0 means (Success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
