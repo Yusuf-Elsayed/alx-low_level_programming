3abe21abefcbe37bfb52f44dbcbd6f42538483b1
Yusuf-Elsayed <yusufElsayed@gmail.com>
2023-08-11 12:05:24 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* cases
-	 * 	if n > 0 --> positive
-	 *	if n < 0 --> negitive
-	 *	n = 0 --> is zero
-	 */
+	/*cases if n > 0 --> positive	 if n < 0 --> negitive	 n = 0 --> is zero*/
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
