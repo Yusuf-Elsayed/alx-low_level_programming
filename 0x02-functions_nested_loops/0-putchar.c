e3d632e75eb8d9f02f86d36091cc4f660b9945c8
Yusuf-Elsayed <yusufElsayed@gmail.com>
2023-08-15 02:35:02 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of a custom '_putchar' function by
+ * printing a string using the 'printf' function. The '_putchar' function is
+ * expected to be defined elsewhere in the code and should handle printing
+ * individual characters.
+ *
+ * Return: Always 0 to indicate successful completion of the program.
+ */
+int main(void)
+{
+	printf("%s\n", "_putchar");
+	return (0);
+}
